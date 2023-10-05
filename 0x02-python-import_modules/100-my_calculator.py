0bce721e2eb2dcae3a6040c55e8c96e8d0fba78f
etharabdelazeem <etharabdelazeem1@gmail.com>
2023-10-05 12:42:14 +0000

    task 100

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -6,7 +6,7 @@ if __name__ == "__main__":
     if (len(argv) != 4):
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         exit(1)
-    else:
+"""    else:
         a = int(argv[1])
         b = int(argv[3])
         match argv[2]:
@@ -20,4 +20,4 @@ if __name__ == "__main__":
                 print("{} / {} = {}".format(a, b, div(a, b)))
             case _:
                 print("Unknown operator. Available operators: +, -, * and /")
-                exit(1)
+                exit(1)"""
