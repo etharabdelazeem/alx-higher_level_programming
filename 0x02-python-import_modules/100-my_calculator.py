4ceba8130e80cb1247038426679d832fb3a6e902
etharabdelazeem <etharabdelazeem1@gmail.com>
2023-10-05 15:57:54 +0000

    task 100

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,16 +1,16 @@
 #!/usr/bin/python3
-from sys import argv
+import sys
 from calculator_1 import add, sub, mul, div
 
 if __name__ == "__main__":
 
-    if (len(argv) != 4):
+    if (len(sys.argv) != 4):
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
-        exit(1)
+        sys.exit(1)
     else:
-        a = int(argv[1])
-        b = int(argv[3])
-        match argv[2]:
+        a = int(sys.argv[1])
+        b = int(sys.argv[3])
+        match sys.argv[2]:
             case "+":
                 print("{} + {} = {}".format(a, b, add(a, b)))
             case "-":
@@ -21,4 +21,4 @@ if __name__ == "__main__":
                 print("{} / {} = {}".format(a, b, div(a, b)))
             case _:
                 print("Unknown operator. Available operators: +, -, * and /")
-                exit(1)
+                sys.exit(1)
