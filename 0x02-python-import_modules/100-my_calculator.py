442208f3ba6670862a7308e5e13fce720f197b99
etharabdelazeem <etharabdelazeem1@gmail.com>
2023-10-05 14:55:30 +0000

    task 100

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -4,21 +4,21 @@ from calculator_1 import add, sub, mul, div
 
 if __name__ == "__main__":
 
-    if (len(argv) != 4):
-        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
-        exit(1)
-    else:
-        a = int(argv[1])
-        b = int(argv[3])
-        match argv[2]:
-            case "+":
-                print("{} + {} = {}".format(a, b, add(a, b)))
-            case "-":
-                print("{} - {} = {}".format(a, b, sub(a, b)))
-            case "*":
-                print("{} * {} = {}".format(a, b, mul(a, b)))
-            case "/":
-                print("{} / {} = {}".format(a, b, div(a, b)))
-            case _:
-                print("Unknown operator. Available operators: +, -, * and /")
-                exit(1)
+        if (len(argv) != 4):
+                    print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+                            exit(1)
+                                else:
+                                            a = int(argv[1])
+                                                    b = int(argv[3])
+                                                            match argv[2]:
+                                                                            case "+":
+                                                                                                print("{} + {} = {}".format(a, b, add(a, b)))
+                                                                                                            case "-":
+                                                                                                                                print("{} - {} = {}".format(a, b, sub(a, b)))
+                                                                                                                                            case "*":
+                                                                                                                                                                print("{} * {} = {}".format(a, b, mul(a, b)))
+                                                                                                                                                                            case "/":
+                                                                                                                                                                                                print("{} / {} = {}".format(a, b, div(a, b)))
+                                                                                                                                                                                                            case _:
+                                                                                                                                                                                                                                print("Unknown operator. Available operators: +, -, * and /")
+                                                                                                                                                                                                                                                exit(1)
