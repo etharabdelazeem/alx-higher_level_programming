f8dff1712f937998dd9a01cf1f75b49ba8374c32
etharabdelazeem <etharabdelazeem1@gmail.com>
2023-10-08 16:41:53 +0000

    task 5

---
+++ 0x03-python-data_structures/5-no_c.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def no_c(my_string):
+    new_string = ""
+    for a in my_string:
+        if (a == "c" or a == "C"):
+            continue
+        new_string = new_string + a
+    return (new_string)
