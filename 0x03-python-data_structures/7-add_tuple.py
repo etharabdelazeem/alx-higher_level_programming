820be41d361d04ff0e18fd3b288d2836e9091452
etharabdelazeem <etharabdelazeem1@gmail.com>
2023-10-08 19:23:14 +0000

    task 7

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,19 +1,20 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
-    if (len(tuple_a) < 1):
-        sum1 = 0 + int(tuple_b[0])
-        sum2 = 0 + int(tuple_b[1])
-    elif (len(tuple_a) == 1):
-        sum1 = int(tuple_a[0]) + int(tuple_b[0])
-        sum2 = 0 + int(tuple_b[1])
-    elif (len(tuple_b) < 1):
-        sum1 = 0 + int(tuple_a[0])
-        sum2 = 0 + int(tuple_a[1])
-    elif (len(tuple_b) == 1):
-        sum1 = int(tuple_a[0]) + int(tuple_b[0])
-        sum2 = 0 + int(tuple_a[1])
-    else:
-        sum1 = int(tuple_a[0]) + int(tuple_b[0])
-        sum2 = int(tuple_a[1]) + int(tuple_b[1])
-    tuple_sum = (sum1, sum2)
-    return (tuple_sum)
+    if(tuple_a or tuple_b):
+        if (len(tuple_a) < 1):
+            sum1 = 0 + int(tuple_b[0])
+            sum2 = 0 + int(tuple_b[1])
+        elif (len(tuple_a) == 1):
+            sum1 = int(tuple_a[0]) + int(tuple_b[0])
+            sum2 = 0 + int(tuple_b[1])
+        elif (len(tuple_b) < 1):
+            sum1 = 0 + int(tuple_a[0])
+            sum2 = 0 + int(tuple_a[1])
+        elif (len(tuple_b) == 1):
+            sum1 = int(tuple_a[0]) + int(tuple_b[0])
+            sum2 = 0 + int(tuple_a[1])
+        else:
+            sum1 = int(tuple_a[0]) + int(tuple_b[0])
+            sum2 = int(tuple_a[1]) + int(tuple_b[1])
+        tuple_sum = (sum1, sum2)
+        return (tuple_sum)
