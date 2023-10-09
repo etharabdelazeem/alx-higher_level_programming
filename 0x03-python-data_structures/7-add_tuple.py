e0b9b51badc2d6e169196d5afa84b43f4ca22e9a
etharabdelazeem <etharabdelazeem1@gmail.com>
2023-10-08 23:24:00 -0700

    task 7

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,20 +1,16 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
-    if(tuple_a and tuple_b):
-        if (len(tuple_a) < 1):
-            sum1 = 0 + int(tuple_b[0])
-            sum2 = 0 + int(tuple_b[1])
-        elif (len(tuple_a) == 1):
-            sum1 = int(tuple_a[0]) + int(tuple_b[0])
-            sum2 = 0 + int(tuple_b[1])
-        elif (len(tuple_b) < 1):
-            sum1 = 0 + int(tuple_a[0])
-            sum2 = 0 + int(tuple_a[1])
-        elif (len(tuple_b) == 1):
-            sum1 = int(tuple_a[0]) + int(tuple_b[0])
-            sum2 = 0 + int(tuple_a[1])
+    if len(tuple_a) < 2:
+        if len(tuple_a) == 0:
+            tuple_a = 0, 0
         else:
-            sum1 = int(tuple_a[0]) + int(tuple_b[0])
-            sum2 = int(tuple_a[1]) + int(tuple_b[1])
-        tuple_sum = int(sum1), int(sum2)
-        return (tuple_sum)
+            tuple_a = tuple_a[0], 0
+
+    if len(tuple_b) < 2:
+        if len(tuple_b) == 0:
+            tuple_b = 0, 0
+        else:
+            tuple_b = tuple_b[0], 0
+
+    added_tuple = tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1]
+    return added_tuple
