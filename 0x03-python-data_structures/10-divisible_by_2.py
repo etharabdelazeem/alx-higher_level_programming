1e3ba6329b28647dd7a4d8e631bf5dab691cc017
etharabdelazeem <etharabdelazeem1@gmail.com>
2023-10-09 00:34:07 -0700

    task 10

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -2,7 +2,7 @@
 def divisible_by_2(my_list=[]):
     div_list = my_list.copy()
     for i in range(len(my_list)):
-        if (i % 2 == 0):
+        if (my_list[i] % 2 == 0):
             div_list[i] = True
         else:
             div_list[i] = False
