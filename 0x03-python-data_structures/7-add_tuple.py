40955f813f1fc48b815be7adc9519188b3e490bb
etharabdelazeem <etharabdelazeem1@gmail.com>
2023-10-08 23:14:45 -0700

    task 7

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
-    if(tuple_a or tuple_b):
+    if(tuple_a and tuple_b):
         if (len(tuple_a) < 1):
             sum1 = 0 + int(tuple_b[0])
             sum2 = 0 + int(tuple_b[1])
