3cc54c5424b01d4f2358f7359f2d8fd89523140a
etharabdelazeem <etharabdelazeem1@gmail.com>
2023-10-08 23:55:58 -0700

    task8

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -3,6 +3,7 @@ def multiple_returns(sentence):
     if (sentence):
         if (sentence == ""):
             first = None
+            length = 0
         else:
             first = sentence[0]
             length = len(sentence)
