d1c4b163b6297ca53eeadb8064640a29e94a4d68
etharabdelazeem <etharabdelazeem1@gmail.com>
2023-10-11 14:10:01 +0000

    task 10

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def best_score(a_dictionary):
+    if a_dictionary is None:
+        return None
+    else:
+        key = list(a_dictionary)[0]
+        best = a_dictionary[key]
+        for a in a_dictionary:
+            if a_dictionary[a] > best:
+                best = a_dictionary[a]
+                key = a
+        return key
