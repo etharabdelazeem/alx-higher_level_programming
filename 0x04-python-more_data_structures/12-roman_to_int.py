80b5b48d5d886a1f10ad0841690f645636956413
etharabdelazeem <etharabdelazeem1@gmail.com>
2023-10-11 16:20:44 +0000

    task 12

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -4,12 +4,13 @@ def roman_to_int(roman_string):
     letter = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
     if roman_string is None:
         return 0
-    for i in range(len(roman_string)):
-        if (roman_string[i] not in list(letter)):
-            return 0
-        elif i + 1 < len(roman_string) and letter[roman_string[i+1]]\
-                > letter[roman_string[i]]:
-            int_sum = int_sum - letter[roman_string[i]]
-        else:
-            int_sum = int_sum + letter[roman_string[i]]
-    return int_sum
+    else:
+        for i in range(len(roman_string)):
+            if (roman_string[i] not in list(letter)):
+                return 0
+            elif i + 1 < len(roman_string) and letter[roman_string[i+1]]\
+                    > letter[roman_string[i]]:
+                int_sum = int_sum - letter[roman_string[i]]
+            else:
+                int_sum = int_sum + letter[roman_string[i]]
+        return int_sum
