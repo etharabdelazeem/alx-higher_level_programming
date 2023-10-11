666d542256513e203d55cb6ae9e4c39ff5f27741
etharabdelazeem <etharabdelazeem1@gmail.com>
2023-10-11 13:56:56 +0000

    task 9

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 def multiply_by_2(a_dictionary):
+    b_dictionary = a_dictionary.copy()
     for a in a_dictionary:
         b_dictionary[a] = a_dictionary[a] * 2
     return b_dictionary
