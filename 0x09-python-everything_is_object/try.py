ca165b3f72cfcb499df826b9aa2b47bc3f5ed7d2
etharabdelazeem <etharabdelazeem1@gmail.com>
2024-02-01 11:02:51 +0000

    +tasks

--- 0x09-python-everything_is_object/try.py
+++ 0x09-python-everything_is_object/try.py
@@ -1,17 +1,5 @@
 #!/usr/bin/python3
 
-def copy_list(l):
-    l1 = l[:]
-    return (l1)
-
-my_list = [1, 2, 3]
-print(my_list)
-
-new_list = copy_list(my_list)
-
-print(my_list)
-print(new_list)
-
-print(new_list == my_list)
-print(new_list is my_list)
-
+a = (1)
+b = (1)
+print(a is b)
