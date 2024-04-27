4eb336f9dd62f82efa13c7f9a3aa6802ce2afd26
etharabdelazeem <etharabdelazeem1@gmail.com>
2024-04-26 17:58:31 -0700

    task 4

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -9,4 +9,4 @@ if __name__ == "__main__":
 
     print("Body response:")
     print("\t- type: {}".format(type(r.text)))
-    print("\t- content: {}".format(r.content))
+    print("\t- content: {}".format(r.text))
