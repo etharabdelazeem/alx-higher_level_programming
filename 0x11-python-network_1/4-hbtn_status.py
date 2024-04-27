f79a8fd62717132ccafe34351258747434d5a94e
etharabdelazeem <etharabdelazeem1@gmail.com>
2024-04-26 17:56:59 -0700

    task 4

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+a Python script that fetches a url and displays the response
+"""
+if __name__ == "__main__":
+    import requests
+
+    r = requests.get("https://alx-intranet.hbtn.io/status")
+
+    print("Body response:")
+    print("\t- type: {}".format(type(r.text)))
+    print("\t- content: {}".format(r.content))
