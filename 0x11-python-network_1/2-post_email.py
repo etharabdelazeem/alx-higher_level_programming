c8cb635fabe8c8ac4e9c3532f2968c2b2583a20c
etharabdelazeem <etharabdelazeem1@gmail.com>
2024-04-26 17:27:04 -0700

    task 2

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -7,7 +7,7 @@ if __name__ == "__main__":
     import urllib.request
     import sys
 
-    values = {'email' : sys.argv[2]}
+    values = {'email': sys.argv[2]}
     data = urllib.parse.urlencode(values)
     data = data.encode('utf-8')
     req = urllib.request.Request(sys.argv[1], data)
